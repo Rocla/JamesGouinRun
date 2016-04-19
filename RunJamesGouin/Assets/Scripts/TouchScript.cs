63da35d927608e9dee515e0b4b5b85f71908a26e
Romain Claret <contact@rocla.ch>
2016-04-19 13:36:43 +0200

    + jump sound

--- RunJamesGouin/Assets/Scripts/TouchScript.cs
+++ RunJamesGouin/Assets/Scripts/TouchScript.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class TouchScript : MonoBehaviour {
 
     bool jumping;
+	public AudioClip jump_sound;
+	AudioSource audio;
 
 	// Use this for initialization
 	void Start () {
         this.jumping = false;
 
         Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+		audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,6 +26,7 @@ public class TouchScript : MonoBehaviour {
         {
             if(!this.jumping)
             {
+				audio.PlayOneShot(jump_sound, 0.7F);
                 this.jumping = true;
                 float i = 0.0f;
 
