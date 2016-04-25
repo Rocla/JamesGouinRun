bd1dce7729a59b2c3513b77f07cf3cf972965ca2
Romain Claret <contact@rocla.ch>
2016-04-26 00:25:45 +0200

    + Main scene (background with mountains&clouds, run game, title, penguin running on the map) + Music Main Scene + Tmp penguin animation (feet) + Background spawner + Ground moving (start to end, move to left) + Love

---
+++ RunJamesGouin/Assets/Scripts/GroundGC.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundGC : MonoBehaviour {
+
+    public Transform groundStart;
+    public Transform groundEnd;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.x < groundEnd.position.x)
+        {
+            float gap = groundEnd.position.x - transform.position.x;
+            transform.position = new Vector3(groundStart.position.x - gap, transform.position.y, transform.position.z);
+        }
+
+	}
+}
---
+++ RunJamesGouin/Assets/Scripts/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public Transform objectToSpawn;
+    public AnimationCurve spawnCurve;
+
+	private float startingTime = 0.0f;
+	private float timeToSpawn = 0.0f;
+    public float curveTime = 30.0f;
+    public float interval = 0.25f;
+
+
+	// Use this for initialization
+	void Start () {
+		startingTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Time.time > timeToSpawn)
+        {
+			Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+
+			float curvePosition = (Time.time - startingTime) / curveTime;
+			if (curvePosition > 1.0f)
+            {
+				curvePosition = 1.0f;
+				startingTime = Time.time;
+            }
+
+			timeToSpawn = Time.time + spawnCurve.Evaluate(curvePosition) + Random.Range(-interval, interval);
+
+        }
+
+	}
+}
