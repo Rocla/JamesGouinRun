a401084344c9a77ff513c523629a392955644365
Steve Visinand <visinandst@gmail.com>
2016-04-30 13:42:02 +0200

    start level 1, design etc...

---
+++ RunJamesGouin/Assets/Scripts/InvisiblerSprite.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvisiblerSprite : MonoBehaviour {
+
+
+	SpriteRenderer sprender;
+	// Use this for initialization
+	void Start () {
+		sprender = gameObject.GetComponent<SpriteRenderer>();
+		sprender.enabled= false;
+	}
+	
+
+}
---
+++ RunJamesGouin/Assets/Scripts/waterMoveScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class waterMoveScript : MonoBehaviour {
+
+
+
+	/// <summary>
+	/// Vitesse de déplacement
+	/// </summary>
+	public Vector2 speed = new Vector2(1, 1);
+
+	/// <summary>
+	/// Stockage du mouvement
+	/// </summary>
+	private Vector2 movement;
+
+	/// <summary>
+	/// Direction
+	/// </summary>
+	public Vector2 direction = new Vector2(-1, 0);
+
+	float timeLimit = 0.5f; // 0.5 seconde
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		movement = new Vector2 (
+			speed.x * direction.x,
+			speed.y * direction.y);
+
+		if (timeLimit > 0) {
+			// Decrease timeLimit.
+			timeLimit -= Time.deltaTime;
+
+		} else {
+			direction.x = direction.x * -1;
+			timeLimit = 0.5f;
+		}
+	}
+		
+	void FixedUpdate()
+	{
+		transform.Translate(movement);
+	}
+
+
+}
